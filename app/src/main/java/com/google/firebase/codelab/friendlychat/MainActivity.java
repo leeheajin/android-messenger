b2460fa1f5a1fbc232db2d5b618c6a925fed957f
leeheajin <00gpwls00@naver.com>
2018-11-07 18:15:48 +0900

    init

--- app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -153,7 +153,7 @@ public class MainActivity extends AppCompatActivity
             }
         }
 
-
+        //
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
